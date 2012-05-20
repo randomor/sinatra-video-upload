eb06319f03ac3a96e0ab69d3772befc5ea7c77c7
randomor <randomor@gmail.com>
2012-05-20 00:08:53 -0500

    added s3 integration and tested

--- video.rb
+++ video.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'aws/s3'
+require 'aws'
 require 'mime/types'
 
 get '/upload' do
@@ -8,31 +8,22 @@ end
 
 post '/upload' do
   unless params[:file] &&
-         (tmpfile = params[:file][:tempfile]) &&
+         (tempfile = params[:file][:tempfile]) &&
          (name = params[:file][:filename])
     @error = "No file selected"
     return haml(:upload)
   end
-  
-  local_file = tmpfile
-  bucket = ENV['S3_BUCKET_NAME']
-  mime_type = MIME::Types.type_for(name) || "application/octet-stream"
 
-  AWS::S3::Base.establish_connection!(
+  
+  s3 = AWS::S3.new(
   :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   )
 
-  base_name = name
+  bucket = s3.buckets[ENV['S3_BUCKET_NAME']]
 
-  puts "Uploading #{local_file} as '#{base_name}' to '#{bucket}'"
-
-  AWS::S3::S3Object.store(
-    base_name,
-    local_file.read,
-    bucket,
-    :content_type => mime_type
-  )
+  obj = bucket.objects["#{name}"]
+  obj.write(tempfile.read)
 
   puts "Uploaded!"
 end
